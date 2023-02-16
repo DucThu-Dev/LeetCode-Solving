49de7118611d22a8cdb98905d7d9adbeda1d32e4
Duc Thu <tranducthu.99@gmail.com>
2023-02-17 01:06:28 +0700

    add: better AddBinary version

--- 67.AddBinary.kt
+++ 67.AddBinary.kt
@@ -50,3 +50,25 @@ fun addBinary(a: String, b: String): String {
 fun addBinary1(a: String, b: String): String {
     return (a.toBigInteger(2) + b.toBigInteger(2)).toString(2)
 }
+
+fun addBinary2(a: String, b: String): String {
+    if (a == "0") return b
+    if (b == "0") return a
+
+    var i = a.length - 1
+    var j = b.length - 1
+    var carry = false
+    val sb = StringBuffer()
+
+    while (i >= 0 || j >= 0) {
+        var sum = if (carry) 1 else 0
+        if (i >= 0) sum += a[i--] - '0'
+        if (j >= 0) sum += b[j--] - '0'
+        sb.append(sum % 2)
+        carry = sum >= 2
+    }
+
+    if (carry) sb.append('1')
+    sb.reverse()
+    return sb.toString()
+}
