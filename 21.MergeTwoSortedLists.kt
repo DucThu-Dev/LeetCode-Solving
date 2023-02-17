96208f45192dc2a4cc86f13ca1f6ddb4cfdc2f00
Duc Thu <tranducthu.99@gmail.com>
2023-02-17 23:41:02 +0700

    add: second try merge two sorted list "

--- 21.MergeTwoSortedLists.kt
+++ 21.MergeTwoSortedLists.kt
@@ -1,12 +1,24 @@
 fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
     if (list2 == null) return list1
     if (list1 == null) return list2
-    val trailNode = mergeTwoLists(list1.next, list2.next)
-    return if (list1.`val` < list2.`val`) {
-        list1.next = list2.also { it.next = trailNode }
-        list1
+    return if (list1.`val` > list2.`val`) {
+        mergeTwoListWithMoreDetail(list2, list1)
     } else {
-        list2.next = list1.also { it.next = trailNode }
-        list2
+        mergeTwoListWithMoreDetail(list1, list2)
+    }
+}
+
+fun mergeTwoListWithMoreDetail(smallerList: ListNode?, greaterList: ListNode?): ListNode? {
+    if (greaterList == null) return smallerList
+    if (smallerList == null) return greaterList
+
+    if (smallerList.next != null && smallerList.next!!.`val` < greaterList.`val`) {
+        mergeTwoListWithMoreDetail(smallerList.next, greaterList)
+        return smallerList
+    } else {
+        var nextNode: ListNode? = mergeTwoLists(smallerList.next, greaterList.next)
+
+        greaterList.next = nextNode
+        return smallerList.apply { next = greaterList }
     }
 }
