0de929faa2206841f0769dc60e8d4b0ee1f89c8b
Duc Thu <tranducthu.99@gmail.com>
2023-02-17 07:41:17 +0700

    add: first solution Minimun distance between BST nodes

--- 783.MinimumDistanceBetweenBSTNodes.kt
+++ 783.MinimumDistanceBetweenBSTNodes.kt
@@ -1,3 +1,10 @@
 fun minDiffInBST(root: TreeNode?): Int {
+    if (root == null || (root.left == null && root.right == null)) return Int.MAX_VALUE
+    var minDiff: Int = Int.MAX_VALUE
+    if (root.left != null) minDiff = Math.abs(root.`val` - root.left!!.`val`)
+    if (root.right != null) {
+        minDiff = Math.min(Math.abs(root.`val` - root.right!!.`val`), minDiff)
 
+    }
+    return Math.min(Math.min(minDiffInBST(root.left), minDiffInBST(root.right)), minDiff)
 }
