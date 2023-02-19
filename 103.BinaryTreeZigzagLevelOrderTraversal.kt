537551fbd4d87a41b0b5f5a3841ae64122bc6e0c
Duc Thu <tranducthu.99@gmail.com>
2023-02-19 09:26:42 +0700

    add: first solution for Binary tree zigzag level order traversal

---
+++ 103.BinaryTreeZigzagLevelOrderTraversal.kt
@@ -0,0 +1,40 @@
+fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
+    if (root == null) return listOf()
+    val res: MutableList<List<Int>> = mutableListOf()
+    var currentLevelNodesQueue: List<TreeNode> = listOf(root)
+    var remNextLevelNodesQueue: MutableList<TreeNode> = mutableListOf()
+    var currentLevel = 0
+
+    while (currentLevelNodesQueue.isNotEmpty()) {
+        forLoop@ for (n in currentLevelNodesQueue) {
+            if (n.left == null && n.right == null) continue@forLoop
+            val resInner = mutableListOf<Int>()
+            if (currentLevel % 2 == 1) {
+                if (n.left != null) {
+                    resInner.add(n.left!!.`val`)
+                    remNextLevelNodesQueue.add(n.left!!)
+                }
+                if (n.right != null) {
+                    resInner.add(n.right!!.`val`)
+                    remNextLevelNodesQueue.add(n.right!!)
+                }
+            } else {
+                if (n.right != null) {
+                    resInner.add(n.right!!.`val`)
+                    remNextLevelNodesQueue.add(n.right!!)
+                }
+                if (n.left != null) {
+                    resInner.add(n.left!!.`val`)
+                    remNextLevelNodesQueue.add(n.left!!)
+                }
+            }
+            res.add(resInner)
+        }
+
+        currentLevelNodesQueue = remNextLevelNodesQueue.reversed()
+        remNextLevelNodesQueue = mutableListOf()
+        currentLevel++
+    }
+
+    return res.toList()
+}
