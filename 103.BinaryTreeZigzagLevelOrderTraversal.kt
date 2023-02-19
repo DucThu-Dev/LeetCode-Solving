1b23df292995d83ad9a61e494cab929593c64d42
Duc Thu <tranducthu.99@gmail.com>
2023-02-19 13:28:15 +0700

    add: practice with best answer BinaryTreeZigzagLevelOrderTraversal

--- 103.BinaryTreeZigzagLevelOrderTraversal.kt
+++ 103.BinaryTreeZigzagLevelOrderTraversal.kt
@@ -30,7 +30,7 @@ fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
             }
         }
 
-        if(resInner.isNotEmpty()) res.add(resInner)
+        if (resInner.isNotEmpty()) res.add(resInner)
         currentLevelNodesQueue = remNextLevelNodesQueue.reversed()
         remNextLevelNodesQueue = mutableListOf()
         currentLevel++
@@ -38,3 +38,47 @@ fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
 
     return res.toList()
 }
+
+/**
+ *  Better version
+ */
+val binaryTreeZigzagLevelOrderTraversalRes: MutableList<MutableList<Int>> = mutableListOf()
+fun zigzagLevelOrder1(root: TreeNode?): List<List<Int>> {
+    zigzagLevelOrderImpl(root, 0)
+    return binaryTreeZigzagLevelOrderTraversalRes
+}
+
+fun zigzagLevelOrderImpl(node: TreeNode?, level: Int) {
+    if (node == null) return;
+    val currentRow = binaryTreeZigzagLevelOrderTraversalRes.getOrElse(level) {
+        mutableListOf<Int>().apply {
+            binaryTreeZigzagLevelOrderTraversalRes.add(level, this)
+        }
+    }
+    if (level % 2 == 0) {
+        currentRow.add(node.`val`)
+    } else {
+        currentRow.add(0, node.`val`)
+    }
+
+    zigzagLevelOrderImpl(node.left, level + 1)
+    zigzagLevelOrderImpl(node.right, level + 1)
+}
+
+
+//val ans = mutableListOf<MutableList<Int>>()
+//fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
+//    dfs(root, 0)
+//    return ans
+//}
+//
+//fun dfs(curr: TreeNode?, index: Int) {
+//    if (curr != null) {
+//        val row = ans.getOrElse(index) { mutableListOf<Int>().also { ans.add(it) } }
+//        if (index % 2 == 0) row.add(curr.`val`)
+//        else row.add(0, curr.`val`)
+//
+//        dfs(curr.left, index + 1)
+//        dfs(curr.right, index + 1)
+//    }
+//}
