c1c0b2680ea9c2095cbaf57e14a6a099bbd35991
Duc Thu <tranducthu.99@gmail.com>
2023-02-19 09:30:12 +0700

    add: fix misunderstanding problem

--- 103.BinaryTreeZigzagLevelOrderTraversal.kt
+++ 103.BinaryTreeZigzagLevelOrderTraversal.kt
@@ -1,14 +1,14 @@
 fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
     if (root == null) return listOf()
-    val res: MutableList<List<Int>> = mutableListOf()
+    val res: MutableList<List<Int>> = mutableListOf(listOf(root.`val`))
     var currentLevelNodesQueue: List<TreeNode> = listOf(root)
     var remNextLevelNodesQueue: MutableList<TreeNode> = mutableListOf()
     var currentLevel = 0
 
     while (currentLevelNodesQueue.isNotEmpty()) {
+        val resInner = mutableListOf<Int>()
         forLoop@ for (n in currentLevelNodesQueue) {
             if (n.left == null && n.right == null) continue@forLoop
-            val resInner = mutableListOf<Int>()
             if (currentLevel % 2 == 1) {
                 if (n.left != null) {
                     resInner.add(n.left!!.`val`)
@@ -28,9 +28,9 @@ fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
                     remNextLevelNodesQueue.add(n.left!!)
                 }
             }
-            res.add(resInner)
         }
 
+        res.add(resInner)
         currentLevelNodesQueue = remNextLevelNodesQueue.reversed()
         remNextLevelNodesQueue = mutableListOf()
         currentLevel++
