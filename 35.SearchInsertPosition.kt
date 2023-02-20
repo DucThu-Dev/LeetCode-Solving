3a9727103875674cfbd276041837eb31d62ac611
Duc Thu <tranducthu.99@gmail.com>
2023-02-20 09:51:34 +0700

    add: better solution using merge sort - type solution

--- 35.SearchInsertPosition.kt
+++ 35.SearchInsertPosition.kt
@@ -6,3 +6,23 @@ fun searchInsert(nums: IntArray, target: Int): Int {
 
     return i
 }
+
+/**
+ * Better solution
+ */
+fun searchInsert1(nums: IntArray, target: Int): Int {
+    var low =0
+    var heigh = nums.size-1
+
+    while(low <= heigh  ){
+        var mid = low +(heigh - low)/2
+        if( nums[mid] == target){
+            return mid
+        }else if(target > nums[mid]){
+            low = mid+1
+        }else{
+            heigh =mid-1
+        }
+    }
+    return low
+}
