57c064f7c50cde1346cb61ccccff5112710e7387
Duc Thu <tranducthu.99@gmail.com>
2023-02-22 00:27:29 +0700

    add: self try 1011

--- 1011.CapacityToShipPackagesWithinDDays.kt
+++ 1011.CapacityToShipPackagesWithinDDays.kt
@@ -1,3 +1,29 @@
 fun shipWithinDays(weights: IntArray, days: Int): Int {
+    var lWeight: Int = weights.max() ?: 0
+    var rWeight: Int = weights.sum() ?: 0
+    if (days == 1) return rWeight
 
+    while (lWeight < rWeight) {
+        val middleWeight = lWeight + (rWeight - lWeight) / 2
+
+        var sumWeight = 0
+        var countDay: Int = 1
+        for (w in weights) {
+            val nextSumWeight = sumWeight + w
+            sumWeight = if (nextSumWeight < middleWeight) {
+                nextSumWeight
+            } else {
+                countDay++
+                w
+            }
+        }
+
+        if (countDay <= days) {
+            rWeight = middleWeight
+        } else {
+            lWeight = middleWeight
+        }
+    }
+
+    return lWeight
 }
