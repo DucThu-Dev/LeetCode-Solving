39a439741c8839411c1f1383df4dd0e80d12cdf7
Duc Thu <tranducthu.99@gmail.com>
2023-02-21 23:00:10 +0700

    add: improve SingleElementInASortedArray

--- 540.SingleElementInASortedArray.kt
+++ 540.SingleElementInASortedArray.kt
@@ -1,31 +1,21 @@
 fun singleNonDuplicate(nums: IntArray): Int {
-    if(nums.size == 1) return nums.first()
-    if(nums.first() != nums[1]) return nums.first()
+    if (nums.size == 1) return nums.first()
+    if (nums.first() != nums[1]) return nums.first()
 
     var startIndex = 0
     var endIndex = nums.lastIndex
 
     while (startIndex != endIndex) {
-        val middleIndex: Int = (endIndex - startIndex) / 2 + startIndex
-        val checkLeftSide = middleIndex % 2 == 1
+        var middleIndex: Int = (endIndex - startIndex) / 2 + startIndex
+        if (middleIndex % 2 == 1) middleIndex - 1
         val middleNum = nums[middleIndex]
 
-        if (checkLeftSide) {
-            if (middleNum == nums[middleIndex - 1]) {
-                startIndex = middleIndex + 1
-            } else if (middleNum == nums[middleIndex + 1]) {
-                endIndex = middleIndex - 1
-            } else {
-                return middleNum
-            }
+        if (middleNum == nums[middleIndex + 1]) {
+            startIndex = middleIndex + 2
+        } else if (middleNum == nums[middleIndex - 1]) {
+            endIndex = middleIndex - 2
         } else {
-            if (middleNum == nums[middleIndex + 1]) {
-                startIndex = middleIndex + 2
-            } else if (middleNum == nums[middleIndex - 1]) {
-                endIndex = middleIndex - 2
-            } else {
-                return middleNum
-            }
+            return middleNum
         }
     }
 
@@ -33,8 +23,8 @@ fun singleNonDuplicate(nums: IntArray): Int {
 }
 
 fun singleNonDuplicate1(nums: IntArray): Int {
-    if(nums.size == 1) return nums.first()
-    if(nums.first() != nums[1]) return nums.first()
+    if (nums.size == 1) return nums.first()
+    if (nums.first() != nums[1]) return nums.first()
     var i = 1
     while (i < nums.lastIndex) {
         if (nums[i] == nums[i - 1] || nums[i] == nums[i + 1]) {
