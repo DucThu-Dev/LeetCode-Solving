d33c859ea6fb7b878b3181f0eabd1ab458eb3db1
Duc Thu <tranducthu.99@gmail.com>
2023-02-23 15:04:48 +0700

    add: try to solve 502.kt

--- 502.IPO.kt
+++ 502.IPO.kt
@@ -1,24 +1,42 @@
-import java.util.PriorityQueue
-
 fun findMaximizedCapital(k: Int, w: Int, profits: IntArray, capital: IntArray): Int {
     var remainProject = k
-    var totalCapital = 0
-    val priorityQueue = PriorityQueue<Int>()
-    for (pIndex in profits.indices) {
-        if (capital[pIndex] < w) {
-            priorityQueue.add(profits[pIndex] - capital[pIndex])
+    val profitsList = profits.toMutableList()
+    val capitalList = capital.toMutableList()
+    var currentW = w
+
+    fun getFeasibleCapital(): Int? {
+        var maxBenefit = 0
+        var maxIndex: Int? = null
+
+        for (pIndex in profitsList.indices) {
+            println("pIndex $pIndex")
+            if (capitalList[pIndex] <= currentW) {
+                if (profitsList[pIndex] > maxBenefit) {
+                    maxBenefit = profitsList[pIndex]
+                    maxIndex = pIndex
+                }
+            }
+        }
+        return if (maxIndex != null) {
+            val result = profitsList[maxIndex]
+            profitsList.removeAt(maxIndex)
+            capitalList.removeAt(maxIndex)
+            result
+        } else {
+            null
         }
     }
 
-    while (remainProject > 0 || priorityQueue.isNotEmpty()) {
-        totalCapital += priorityQueue.poll() ?: 0
-        remainProject --
+    while (remainProject > 0 && profitsList.isNotEmpty()) {
+        println("Current profitsList length is ${profitsList.size} ${capitalList.size}")
+        currentW += getFeasibleCapital() ?: 0
+        remainProject--
     }
 
-    return totalCapital
+    return currentW
 }
 
 fun main() {
-    val result = findMaximizedCapital(2, 0, intArrayOf(1,2,3), intArrayOf(0,1,1))
+    val result = findMaximizedCapital(2, 0, intArrayOf(1, 2, 3), intArrayOf(0, 1, 1))
     println(result)
 }
