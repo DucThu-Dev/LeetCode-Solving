524cf397003f22ed9cc8638f7e7e7714da8babeb
Duc Thu <tranducthu.99@gmail.com>
2023-02-25 13:00:30 +0700

    add: solution for 27

---
+++ 27.RemoveElement.kt
@@ -0,0 +1,11 @@
+fun removeElement(nums: IntArray, `val`: Int): Int {
+    var pointer = 0
+
+    for (n in nums) {
+        if (n != `val`) {
+            nums[pointer] = n
+            pointer++
+        }
+    }
+    return pointer
+}
