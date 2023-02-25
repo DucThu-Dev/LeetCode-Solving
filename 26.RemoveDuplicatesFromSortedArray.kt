4a720af4e4f61ceddd9e89ba9e51a3daa9ff6dc9
Duc Thu <tranducthu.99@gmail.com>
2023-02-25 12:02:26 +0700

    add: accepted solution for 26"

--- 26.RemoveDuplicatesFromSortedArray.kt
+++ 26.RemoveDuplicatesFromSortedArray.kt
@@ -1,3 +1,19 @@
-//fun removeDuplicates(nums: IntArray): Int {
-//
-//}
+fun removeDuplicates(nums: IntArray): Int {
+    val indexUniqueNums = mutableListOf<Int>(0)
+    var currentNum = nums[0]
+
+    for (i in 1..nums.lastIndex) {
+        if (currentNum != nums[i]) {
+            indexUniqueNums.add(i)
+            currentNum = nums[i]
+        }
+    }
+
+    val result = indexUniqueNums.size
+
+    for ((currentIndex, i) in indexUniqueNums.withIndex()) {
+        nums[currentIndex] = nums[i]
+    }
+
+    return result
+}
