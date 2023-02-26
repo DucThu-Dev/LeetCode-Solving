af012ec3e78cefff5f687b33777b11daf7a5fcc9
Duc Thu <tranducthu.99@gmail.com>
2023-02-27 01:51:09 +0700

    add: apply dynamic programming for 72

--- L1_999/72.EditDistance.kt
+++ L1_999/72.EditDistance.kt
@@ -1,9 +1,56 @@
-/// Memoization - Top-Down Dynamic Programming
+fun main() {
+//    minDistanceRecursive("dinitrophenylhydrazine", "benzalphenylhydrazone")
+//    minDistanceRecursive("abcde", "axbxcxdxe")
 
+    val result = minDistanceDynamicProgramming("dinitrophenylhydrazine", "benzalphenylhydrazone")
+    println("Result is $result")
+
+}
+
+
+/// Memoization - Top-Down Dynamic Programming
+fun minDistanceDynamicProgramming(word1: String, word2: String): Int {
+    val memo: Array<Array<Int?>> = Array(word1.length + 1) {
+        Array<Int?>(word2.length + 1) {
+            null
+        }
+    }
+
+    return minDistanceRecurDynamicProgramming(word1, word2, word1.length, word2.length, memo)
+}
+
+fun minDistanceRecurDynamicProgramming(
+    word1: String,
+    word2: String,
+    word1Index: Int,
+    word2Index: Int,
+    memo: Array<Array<Int?>>
+): Int {
+    if (word1Index == 0) return word2Index
+    if (word2Index == 0) return word1Index
+
+    if (memo[word1Index][word2Index] != null) {
+        return memo[word1Index][word2Index]!!
+    }
+
+    var minDistanceResult: Int = 0
+
+    if (word1[word1Index - 1] == word2[word2Index - 1]) {
+        minDistanceResult = minDistanceRecurDynamicProgramming(word1, word2, word1Index - 1, word2Index - 1, memo)
+    } else {
+        val deleteResult = minDistanceRecurDynamicProgramming(word1, word2, word1Index - 1, word2Index, memo)
+        val insertResult = minDistanceRecurDynamicProgramming(word1, word2, word1Index, word2Index - 1, memo)
+        val replaceResult = minDistanceRecurDynamicProgramming(word1, word2, word1Index - 1, word2Index - 1, memo)
+        minDistanceResult = Math.min(deleteResult, Math.min(insertResult, replaceResult)) + 1
+    }
+    println("MinDistanceResult is $minDistanceResult")
+    memo[word1Index][word2Index] = minDistanceResult
+    return minDistanceResult
+}
 
 /// Recursion approach - out of time limit
 fun minDistanceRecursive(word1: String, word2: String): Int {
-    return minDistanceRecur(word1, word2, word1.length, word2.length)
+    return minDistanceRecur(word1, word2, word1.length, word2.length).apply { println("Final result is $this") }
 }
 
 fun minDistanceRecur(word1: String, word2: String, word1Index: Int, word2Index: Int): Int {
@@ -27,12 +74,6 @@ fun minDistanceRecur(word1: String, word2: String, word1Index: Int, word2Index: 
         }
     }
 }
-
-fun main() {
-    minDistanceRecursive("dinitrophenylhydrazine", "benzalphenylhydrazone")
-    minDistanceRecursive("abcde", "axbxcxdxe")
-}
-
 
 /// The original legacy solution but do not meet all test cases.
 /// NOTE: Didn't accepted answer
