37cf0a7d0e7f4f5100bc764d4e5dd23883a7738e
Duc Thu <tranducthu.99@gmail.com>
2023-02-27 01:06:58 +0700

    add: Recursive approach on 72 from LeetCode Editorial

--- L1_999/72.EditDistance.kt
+++ L1_999/72.EditDistance.kt
@@ -1,11 +1,52 @@
-fun minDistance(word1: String, word2: String): Int {
+/// Memoization - Top-Down Dynamic Programming
+
+
+/// Recursion approach - out of time limit
+fun minDistanceRecursive(word1: String, word2: String): Int {
+    return minDistanceRecur(word1, word2, word1.length, word2.length)
+}
+
+fun minDistanceRecur(word1: String, word2: String, word1Index: Int, word2Index: Int): Int {
+    if (word1Index == 0) return word2Index
+    if (word2Index == 0) return word1Index
+
+    println("Working on word1 ${word1.substring(0, word1Index - 1)}")
+    println("Working on word2 ${word2.substring(0, word2Index - 1)}")
+
+    if (word1[word1Index - 1] == word2[word2Index - 1]) {
+        return minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1).apply {
+            println("Word1[${word1Index - 1}] equals Word2[${word2Index - 1}] equals ${word1[word1Index - 1]}")
+        }
+    } else {
+        val insertOperation = minDistanceRecur(word1, word2, word1Index, word2Index - 1)
+        val deleteOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index)
+        val replaceOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1)
+
+        return (Math.min(insertOperation, Math.min(deleteOperation, replaceOperation)) + 1).apply {
+            println("The result of compare 3 value is $this")
+        }
+    }
+}
+
+fun main() {
+    minDistanceRecursive("dinitrophenylhydrazine", "benzalphenylhydrazone")
+    minDistanceRecursive("abcde", "axbxcxdxe")
+}
+
+
+/// The original legacy solution but do not meet all test cases.
+/// NOTE: Didn't accepted answer
+fun minDistanceLegacy(word1: String, word2: String): Int {
     if (word1 == word2) return 0
     if (word1 in word2 || word2 in word1) return Math.abs(word1.length - word2.length)
 
-    return Math.min(minDistanceLeftToRight(word1, word2), minDistanceLeftToRight(word1.reversed(), word2.reversed()))
+    return Math.min(
+        minDistanceLeftToRightLegacy(word1, word2),
+        minDistanceLeftToRightLegacy(word1.reversed(), word2.reversed())
+    )
 }
 
-fun minDistanceLeftToRight(word1: String, word2: String): Int {
+fun minDistanceLeftToRightLegacy(word1: String, word2: String): Int {
     // Pair with index of word1 - word2
     var firstMatchIndex: Pair<Int, Int>? = null
     var lastMatchIndex: Pair<Int, Int>? = null
