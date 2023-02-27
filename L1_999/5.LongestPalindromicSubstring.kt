f185e38625b0c25bc84ef9c26486019c9081e39d
Duc Thu <tranducthu.99@gmail.com>
2023-02-27 23:29:08 +0700

    add: Replay best solution for 5

--- L1_999/5.LongestPalindromicSubstring.kt
+++ L1_999/5.LongestPalindromicSubstring.kt
@@ -1,3 +1,42 @@
+/**
+ *  Best solution
+ */
+fun longestPalindromeBestSolution(s: String): String {
+    if (s.isEmpty()) return ""
+    if (s.length == 1) return s
+
+    var start = 0
+    var end = 0
+
+    for (i in 0 until s.lastIndex) {
+        val odd = expandHorizontalCheck(s, i, i)
+        val even = expandHorizontalCheck(s, i, i + 1)
+
+        val effectiveLen = Math.max(odd, even)
+
+        if (effectiveLen > end - start + 1) {
+            start = i - (effectiveLen - 1) / 2
+            end = i + effectiveLen / 2
+        }
+    }
+
+    return s.substring(start, end + 1)
+}
+
+fun expandHorizontalCheck(s: String, start: Int, end: Int): Int {
+    var left = start
+    var right = end
+    while (left >= 0 && right <= s.lastIndex && s[left] == s[right]) {
+        left--
+        right++
+    }
+    return right - left - 1
+}
+
+
+/**
+ *  My original solution
+ */
 fun longestPalindrome(s: String): String {
     if (s.isEmpty()) return ""
     if (s.length == 1) return s
@@ -5,7 +44,8 @@ fun longestPalindrome(s: String): String {
     var longest = ""
 
     for (i in 0 until s.lastIndex) {
-        for (j in i..s.lastIndex) {
+        val nextStartJ = i + longest.length
+        for (j in nextStartJ..s.lastIndex) {
             if (s[i] == s[j] && j - i + 1 > longest.length) {
                 val isPal = isPalindromic(s.substring(i, j + 1))
                 if (isPal) {
@@ -21,3 +61,4 @@ fun longestPalindrome(s: String): String {
 fun isPalindromic(text: String): Boolean {
     return text.equals(text.reversed())
 }
+
