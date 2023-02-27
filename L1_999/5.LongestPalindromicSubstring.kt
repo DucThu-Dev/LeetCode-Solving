098da95cab0e3f7ad9dc5da7aec07301d7056c35
Duc Thu <tranducthu.99@gmail.com>
2023-02-27 22:51:09 +0700

    add: accepted first attempt 5

---
+++ L1_999/5.LongestPalindromicSubstring.kt
@@ -0,0 +1,23 @@
+fun longestPalindrome(s: String): String {
+    if (s.isEmpty()) return ""
+    if (s.length == 1) return s
+
+    var longest = ""
+
+    for (i in 0 until s.lastIndex) {
+        for (j in i + 1..s.lastIndex) {
+            if (s[i] == s[j] && j - i + 1 > longest.length) {
+                val isPal = isPalindromic(s.substring(i, j + 1))
+                if (isPal) {
+                    longest = s.substring(i, j + 1)
+                }
+            }
+        }
+    }
+
+    return longest
+}
+
+fun isPalindromic(text: String): Boolean {
+    return text.equals(text.reversed())
+}
