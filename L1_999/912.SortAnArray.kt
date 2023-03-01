7ff1bd853caee79bcb170d3c5c10232e417f9681
Duc Thu <tranducthu.99@gmail.com>
2023-03-01 12:49:29 +0700

    add: improve memory usage and optimize logic but still not accepted 912

--- L1_999/912.SortAnArray.kt
+++ L1_999/912.SortAnArray.kt
@@ -1,5 +1,30 @@
 fun sortArray(nums: IntArray): IntArray {
     if (nums.isEmpty() || nums.size == 1) return nums
+    val nums = nums.toMutableList()
+    var currentIndex = 0
+    var pivotIndex = nums.size / 2
+    val pivotValue = nums[pivotIndex]
+    while (currentIndex < nums.size) {
+        if (currentIndex < pivotIndex && nums[currentIndex] >= pivotValue) {
+            nums.add(pivotIndex, nums.removeAt(currentIndex))
+            pivotIndex--
+        } else if (currentIndex > pivotIndex && nums[currentIndex] < pivotValue) {
+            nums.add(pivotIndex, nums.removeAt(currentIndex))
+            pivotIndex++
+        } else {
+            currentIndex++
+        }
+    }
+
+    return intArrayOf(
+        *sortArray(nums.subList(0, pivotIndex).toIntArray()),
+        pivotValue,
+        * sortArray(nums.subList(pivotIndex + 1, nums.size).toIntArray())
+    )
+}
+
+fun sortArrayOriginal(nums: IntArray): IntArray {
+    if (nums.isEmpty() || nums.size == 1) return nums
     val leftNums = mutableListOf<Int>()
     val rightNums = mutableListOf<Int>()
 
