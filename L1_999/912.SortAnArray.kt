4d859cb984ac574c0089847f1d3e248559dd3252
Duc Thu <tranducthu.99@gmail.com>
2023-03-01 12:38:04 +0700

    add: solution pass run but not accepted due to timeout 912

--- L1_999/912.SortAnArray.kt
+++ L1_999/912.SortAnArray.kt
@@ -1,35 +1,19 @@
 fun sortArray(nums: IntArray): IntArray {
     if (nums.isEmpty() || nums.size == 1) return nums
-    val nums = nums.toMutableList()
+    val leftNums = mutableListOf<Int>()
+    val rightNums = mutableListOf<Int>()
 
     val pivotIndex = nums.size / 2
-    var pivotPair = Pair(pivotIndex, nums[pivotIndex])
+    val pivotValue = nums[pivotIndex]
 
     for (i in 0..nums.lastIndex) {
-        if (i < pivotPair.first && nums[i] >= pivotPair.second) {
-            val value = nums.removeAt(i)
-            nums.add(pivotPair.first, value)
-            pivotPair = (pivotPair.first - 1) to pivotPair.second
-        } else if (i > pivotPair.first && nums[i] < pivotPair.second) {
-            val value = nums.removeAt(i)
-            nums.add(pivotPair.first, value)
-            pivotPair = (pivotPair.first + 1) to pivotPair.second
+        if (i != pivotIndex) {
+            if (nums[i] < pivotValue)
+                leftNums.add(nums[i])
+            else
+                rightNums.add(nums[i])
         }
-
     }
 
-    val result1 = sortArray(nums.subList(0, pivotPair.first).toIntArray())
-    val result2 = sortArray(nums.subList(pivotPair.first + 1, nums.size).toIntArray())
-
-    return intArrayOf(*result1, pivotPair.second, *result2).also {
-        println("Current nums: $nums")
-    }
-}
-
-fun main() {
-//    sortArray(intArrayOf(5, 1, 1, 2, 0, 0))
-    val nums = mutableListOf(5, 1, 2)
-    nums.remove(1)
-    nums.add(2, 5)
-    println(nums)
+    return intArrayOf(*sortArray(leftNums.toIntArray()), pivotValue, *sortArray(rightNums.toIntArray()))
 }
