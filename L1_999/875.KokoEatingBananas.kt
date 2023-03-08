a47761465841c4e533719cc375dfc8b3f68e59d6
Duc Thu <tranducthu.99@gmail.com>
2023-03-08 12:01:01 +0700

    add: solution accepted for 875

--- L1_999/875.KokoEatingBananas.kt
+++ L1_999/875.KokoEatingBananas.kt
@@ -1,4 +1,20 @@
-//fun minEatingSpeed(piles: IntArray, h: Int): Int {
-//    var leftSpeed = 1
-//    var rightSpeed = piles.sum()
-//}
+fun minEatingSpeed(piles: IntArray, h: Int): Int {
+    fun feasible(k: Long): Boolean {
+        var rem = 0L
+        for (i in piles) {
+            rem += i / k
+            if (i % k > 0) rem++
+            if (rem > h) return false;
+        }
+        return true
+    }
+
+    var leftSpeed = 1L
+    var rightSpeed = piles.map { it.toLong() }.sum()
+    while (leftSpeed < rightSpeed) {
+        val middle = leftSpeed + (rightSpeed - leftSpeed) / 2
+        if (feasible(middle)) rightSpeed = middle else leftSpeed = middle + 1
+    }
+
+    return leftSpeed.toInt()
+}
