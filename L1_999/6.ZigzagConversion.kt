b2c9d16cc5ab0c9e060b2d9dbcfcca9b725c9acc
Duc Thu <tranducthu.99@gmail.com>
2023-03-08 12:59:19 +0700

    add: solution accepted 6

--- L1_999/6.ZigzagConversion.kt
+++ L1_999/6.ZigzagConversion.kt
@@ -1,13 +1,31 @@
-//import java.lang.StringBuilder
-//
-//fun convert(s: String, numRows: Int): String {
-//    if (numRows == 1) return s
-//
-//    val sb = StringBuilder()
-//
-//    for (i in 0 until numRows) {
-//        if (i == 0 || i == numRows - 1) {
-//
-//        }
-//    }
-//}
+import java.lang.StringBuilder
+
+fun convert(s: String, numRows: Int): String {
+    val sb = StringBuilder()
+    if (numRows == 1) return s
+    val rem: MutableList<MutableList<Char>> = MutableList(numRows) { mutableListOf<Char>() }
+    val phaseNodeCount = numRows * 2 - 2
+
+    var rowIndex = 0
+    for (i in s.indices) {
+        val delta = i % phaseNodeCount
+        if (delta < numRows) {
+            rowIndex = delta
+        } else {
+            rowIndex = numRows - 2 - (delta - numRows)
+        }
+        rem[rowIndex].add(s[i])
+    }
+
+    for (l in rem) {
+        for (c in l) {
+            sb.append(c)
+        }
+    }
+
+    return sb.toString()
+}
+
+fun main() {
+    convert("PAYPALISHIRING", 3)
+}
