94a91cc3ed061b80964f7d7dfc20fbe0a544da29
Duc Thu <tranducthu.99@gmail.com>
2023-03-11 23:23:51 +0700

    add: practice fast and slow pointer 109

--- L1_999/109.ConvertSortedListToBinarySearchTree.kt
+++ L1_999/109.ConvertSortedListToBinarySearchTree.kt
@@ -1,9 +1,41 @@
-//class ConvertSortedListToBinarySearchTree {
-//    fun sortedListToBST(head: ListNode?): TreeNode? {
-//
-//    }
-//}
-//
-//class AVLTreeNode : TreeNode() {
-//
-//}
+import com.sun.source.tree.Tree
+
+fun sortedListToBST(head: ListNode?): TreeNode? {
+    head ?: return null
+    head.next ?: return TreeNode(head.`val`)
+    var fast = head
+    var slow = head
+    var pre: ListNode? = null
+    while (fast != null && fast.next != null) {
+        pre = slow
+        slow = slow?.next
+        fast = fast?.next?.next
+    }
+
+    pre?.next = null
+    return TreeNode(slow?.`val` ?: -1).apply {
+        left = sortedListToBST(head)
+        right = sortedListToBST(slow?.next)
+    }
+}
+
+fun sortedListToBSTPractice(head: ListNode?): TreeNode? {
+    head ?: return null
+    head.next ?: return TreeNode(head.`val`)
+
+    var fast = head
+    var slow = head
+    var pre: ListNode? = null
+    while (fast?.next != null) {
+        pre = slow
+        slow = slow?.next
+        fast = fast.next?.next
+    }
+
+    pre?.next = null
+
+    return TreeNode(slow?.`val`!!).apply {
+        left = sortedListToBSTPractice(head)
+        right = sortedListToBSTPractice(slow.next)
+    }
+}
