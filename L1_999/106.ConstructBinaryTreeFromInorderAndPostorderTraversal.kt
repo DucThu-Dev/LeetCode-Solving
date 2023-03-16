b720f320dccb45a0a259525763d2e44a432631d0
Duc Thu <tranducthu.99@gmail.com>
2023-03-16 12:34:11 +0700

    add: solution accepted 106

---
+++ L1_999/106.ConstructBinaryTreeFromInorderAndPostorderTraversal.kt
@@ -0,0 +1,15 @@
+fun buildTree(inorder: IntArray, postorder: IntArray): TreeNode? {
+    if(inorder.isEmpty()) return null
+    val rootVal = postorder.last()
+    val indexOfRootNodeIO = inorder.indexOf(rootVal)
+    val leftIO = inorder.copyOfRange(0, indexOfRootNodeIO)
+    val leftPO = postorder.copyOfRange(0, indexOfRootNodeIO)
+    val rightIO = inorder.copyOfRange(indexOfRootNodeIO + 1, inorder.size)
+    val rightPO = postorder.copyOfRange(indexOfRootNodeIO, postorder.lastIndex)
+
+    val root = TreeNode(rootVal)
+    root.left = buildTree(leftIO, leftPO)
+    root.right = buildTree(rightIO, rightPO)
+
+    return root
+}
