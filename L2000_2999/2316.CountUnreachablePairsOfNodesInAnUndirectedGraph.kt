03f16cd39668f068dbabcc9cc2ce02548cbbda5c
Duc Thu <tranducthu.99@gmail.com>
2023-03-25 12:37:38 +0700

    add: solution accpeted 2316

--- L2000_2999/2316.CountUnreachablePairsOfNodesInAnUndirectedGraph.kt
+++ L2000_2999/2316.CountUnreachablePairsOfNodesInAnUndirectedGraph.kt
@@ -41,3 +41,34 @@ fun countPairs(n: Int, edges: Array<IntArray>): Long {
 fun main() {
     countPairs(3, arrayOf(intArrayOf(0, 1), intArrayOf(0, 2), intArrayOf(1, 2)))
 }
+
+fun countPairsDFS(n: Int, edges: Array<IntArray>): Long {
+    val graph: MutableMap<Int, MutableList<Int>> = mutableMapOf()
+    for (i in 0 until n) graph[i] = mutableListOf()
+
+    for (e in edges) {
+        graph[e[0]]!!.add(e[1])
+        graph[e[1]]!!.add(e[0])
+    }
+
+    var result: ULong = 0u
+    val graphVisitedNodes = MutableList(n) { false }
+
+    fun dfs(node: Int): Int {
+        graphVisitedNodes[node] = true
+        var cntr = 1
+        graph[node]!!.forEach {
+            cntr += if (graphVisitedNodes[it]) 0 else dfs(it)
+        }
+
+        return cntr
+    }
+
+    for (i in 0 until n) {
+        if (graphVisitedNodes[i]) continue
+        val c = dfs(i)
+        result += (c.toULong() * (n.toULong() - c.toULong())).toULong()
+    }
+
+    return (result / 2u).toLong()
+}
