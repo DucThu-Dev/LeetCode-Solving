8bab1e0d13715007e4de9fd6293870f768214a61
Duc Thu <tranducthu.99@gmail.com>
2023-03-26 23:42:58 +0700

    add: practice with 2360

--- L2000_2999/2360.LongestCycleInAGraph.kt
+++ L2000_2999/2360.LongestCycleInAGraph.kt
@@ -17,7 +17,7 @@ fun longestCycle(edges: IntArray): Int {
 
             if (edges[node] >= 0) {
                 val next = edges[node]
-                edges[node] = -1
+//                edges[node] = -1
                 node = next
             } else {
                 break
@@ -29,6 +29,44 @@ fun longestCycle(edges: IntArray): Int {
         if (edges[i] >= 0) {
             go(i)
         }
+    }
+
+    return ans
+}
+
+fun main() {
+    longestCycle(intArrayOf(3, 3, 4, 2, 3))
+}
+
+fun longestCyclePractice(edges: IntArray): Int {
+    var ans = -1
+
+    fun traversal(node: Int) {
+        var nextNode = node
+        val steps = mutableMapOf<Int, Int>()
+        var cntr = 0
+
+        while (true) {
+            cntr++
+            if (steps[nextNode] != null) {
+                ans = ans.coerceAtLeast(cntr - steps[nextNode]!!)
+                break
+            }
+
+            steps[nextNode] = cntr
+
+            if (edges[nextNode] != -1) {
+                val next = edges[nextNode]
+                edges[nextNode] = -1
+                nextNode = next
+            } else {
+                break
+            }
+        }
+    }
+
+    for (i in edges.indices) {
+        if (edges[i] != -1) traversal(i)
     }
 
     return ans
