245d3e6b8b017dd469cfe1bb908e777427903910
Duc Thu <tranducthu.99@gmail.com>
2023-04-02 08:28:22 +0700

    add: optimized 2300 with binary search

--- L2000_2999/2300.SuccessfulPairsOfSpellsAndPotions.kt
+++ L2000_2999/2300.SuccessfulPairsOfSpellsAndPotions.kt
@@ -2,6 +2,23 @@ fun successfulPairs(spells: IntArray, potions: IntArray, success: Long): IntArra
     potions.sort()
     val counted: MutableMap<Int, Int> = mutableMapOf()
     val pair = IntArray(spells.size) { 0 }
+
+    fun binarySearch(target: Long): Int {
+        if(target > potions.last()) return -1
+        var start = 0
+        var end = potions.lastIndex
+        while (start < end) {
+            val middle = start + (end - start) / 2
+            if (potions[middle] < target) {
+                start = middle + 1
+            } else {
+                end = middle
+            }
+        }
+        return if (potions[start] >= target) start
+        else -1
+    }
+
     for (i in spells.indices) {
         if (counted.containsKey(spells[i])) {
             pair[i] = counted[spells[i]]!!
@@ -9,7 +26,7 @@ fun successfulPairs(spells: IntArray, potions: IntArray, success: Long): IntArra
         }
         var target: Long = success / spells[i]
         if (success % spells[i] != 0L) target++
-        val indexOfFirstSuccess = potions.indexOfFirst { it >= target }
+        val indexOfFirstSuccess = binarySearch(target)
         val result = if (indexOfFirstSuccess != -1) potions.size - indexOfFirstSuccess else 0
         pair[i] = result
         counted[spells[i]] = result
