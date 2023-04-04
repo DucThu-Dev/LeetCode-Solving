65a29d9f0354d345cc0d7edd70dbafaceadad4f5
Duc Thu <tranducthu.99@gmail.com>
2023-04-04 23:53:04 +0700

    add: Abstract Factory Design Pattern with Dart

---
+++ design_pattern/dart/abstract_factory.dart
@@ -0,0 +1,40 @@
+abstract class Button {}
+
+class WinButton extends Button {}
+
+class MacButton extends Button {}
+
+abstract class CheckBox {}
+
+class WinCheckBox extends CheckBox {}
+
+class MacCheckBox extends CheckBox {}
+
+abstract class GUIFactory {
+  Button createButton();
+
+  CheckBox createCheckBox();
+}
+
+class WinFactory implements GUIFactory {
+  @override
+  Button createButton() => WinButton();
+
+  @override
+  CheckBox createCheckBox() => WinCheckBox();
+}
+
+class MacFactory implements GUIFactory {
+  @override
+  Button createButton() => MacButton();
+
+  @override
+  CheckBox createCheckBox() => MacCheckBox()
+}
+
+void main() {
+  final configOS = 'MAC';
+  final factory = configOS == 'MAC' ? MacFactory() : WinFactory();
+  final button = factory.createButton();
+  final checkBox = factory.createCheckBox();
+}
