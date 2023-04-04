e64a6cac9ecc7d48e9c08ef91e042ee43aecd11d
Duc Thu <tranducthu.99@gmail.com>
2023-04-04 23:05:47 +0700

    add: Factory Method with Kotlin

---
+++ design_pattern/kotlin/FactoryMethod.kt
@@ -0,0 +1,41 @@
+package design_pattern.kotlin
+
+abstract class Animal {
+    abstract fun makeNoise()
+}
+
+class Dog : Animal() {
+    override fun makeNoise() {
+        println("Gau Gau")
+    }
+}
+
+class Cat : Animal() {
+    override fun makeNoise() {
+        println("Meow Meow")
+    }
+}
+
+class Chicken : Animal() {
+    override fun makeNoise() {
+        println("O o o o")
+    }
+}
+
+enum class AnimalType {
+    DOG, CAT, CHICKEN
+}
+
+object AnimalFactory {
+    fun createAnimal(type: AnimalType): Animal {
+        return when (type) {
+            AnimalType.DOG -> Dog()
+            AnimalType.CAT -> Cat()
+            AnimalType.CHICKEN -> Chicken()
+        }
+    }
+}
+
+fun main() {
+    AnimalFactory.createAnimal(AnimalType.CHICKEN).makeNoise()
+}
