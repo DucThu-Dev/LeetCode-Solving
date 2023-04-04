062b5a7c05cd7f20a5c9d03a15063f3af7b420be
Duc Thu <tranducthu.99@gmail.com>
2023-04-04 23:45:22 +0700

    add: abstract factory in Kotlin

---
+++ design_pattern/kotlin/AbstractFactory.kt
@@ -0,0 +1,50 @@
+package design_pattern.kotlin
+
+/**
+ * [Source](https://refactoring.guru/design-patterns/abstract-factory)
+ */
+
+
+abstract class Button
+
+class WinButton : Button()
+
+class MacButton : Button()
+
+abstract class CheckBox
+
+class WinCheckBox : CheckBox()
+
+class MacCheckBox : CheckBox()
+
+interface GUIFactory {
+    fun createButton(): Button
+
+    fun createCheckBox(): CheckBox
+}
+
+class WinFactory : GUIFactory {
+    override fun createButton(): Button {
+        return WinButton()
+    }
+
+    override fun createCheckBox(): CheckBox {
+        return WinCheckBox()
+    }
+}
+
+class MacFactory : GUIFactory {
+    override fun createButton(): Button {
+        return MacButton()
+    }
+
+    override fun createCheckBox(): CheckBox {
+        return MacCheckBox()
+    }
+}
+
+fun main() {
+    val factory: GUIFactory = WinFactory()
+    val button = factory.createButton()
+    val checkBox = factory.createCheckBox()
+}
