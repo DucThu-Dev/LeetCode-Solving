574f6c03d43aa1ae88488083aa6b9cafc3eeb620
Duc Thu <tranducthu.99@gmail.com>
2023-09-12 14:37:06 +0700

    [LeetCode] Best solution for 1647

--- L1000_1999/1647.MinimumDeletionsToMakeCharacterFrequenciesUnique.ts
+++ L1000_1999/1647.MinimumDeletionsToMakeCharacterFrequenciesUnique.ts
@@ -34,3 +34,47 @@ function minDeletions(s: string): number {
 
   return result;
 };
+
+/// Best solution
+function minDeletionsCloned(s: string): number {
+  const freq = new Array(26).fill(0);
+
+  for (let i = 0; i < s.length; i++) {
+    freq[s.charCodeAt(i) - 97]++;
+  }
+
+  const hash = new Map<number, number>();
+
+  for (let f of freq) {
+    updateMap(f, 1);
+  }
+
+  let count = 0;
+  const hashLastSolve = new Map<number, number>();
+  for (let key of hash.keys()) {
+    while (hash.get(key)! > 1) {
+      let search = key - 1;
+      if (hashLastSolve.has(key)) {
+        search = hashLastSolve.get(key)!;
+      }
+      while (hash.has(search) && hash.get(search)! > 0 && search > 0) {
+        search--;
+      }
+      count += key - search;
+      hashLastSolve.set(key, search);
+      updateMap(search, 1);
+      updateMap(key, -1);
+    }
+  }
+
+  function updateMap(key: number, by: number) {
+    if (key === 0) return;
+    if (hash.has(key)) {
+      hash.set(key, hash.get(key)! + by);
+    } else {
+      hash.set(key, 1);
+    }
+  }
+
+  return count;
+}
