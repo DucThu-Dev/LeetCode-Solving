fdaf32ec26b95d26f3f547d5d01def9d88193df3
Duc Thu <tranducthu.99@gmail.com>
2023-09-13 17:46:53 +0700

    [LeetCode] Solution 135 best cloned

--- L1_999/135.Candy.ts
+++ L1_999/135.Candy.ts
@@ -20,3 +20,21 @@ function candy(ratings: number[]): number {
 
   return candies.reduce((a, b) => a + b);
 };
+
+function candyCloned(ratings: number[]): number {
+  const candies = new Array(ratings.length).fill(1);
+  const n = ratings.length;
+  for (let i = 1; i < n; i++) {
+    if (ratings[i] > ratings[i - 1]) {
+      candies[i] = candies[i - 1] + 1;
+    }
+  }
+
+  for (let i = n - 2; i >= 0; i--) {
+    if (ratings[i] > ratings[i + 1]) {
+      candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+    }
+  }
+
+  return candies.reduce((a, b) => a + b);
+};
