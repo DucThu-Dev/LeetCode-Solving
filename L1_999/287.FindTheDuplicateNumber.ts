3a28f7e379dabf40266cd31103d2d138c1ca52df
Duc Thu <tranducthu.99@gmail.com>
2023-09-19 22:09:52 +0700

    [LeetCode] Practice 287

--- L1_999/287.FindTheDuplicateNumber.ts
+++ L1_999/287.FindTheDuplicateNumber.ts
@@ -1,6 +1,23 @@
 function findDuplicate(nums: number[]): number {
   let slow = 0;
   let fast = 0;
+  while (true) {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+    if (slow === fast) break;
+  }
+
+  let beginCycle = 0;
+  while (true) {
+    slow = nums[slow];
+    beginCycle = nums[beginCycle];
+    if (slow === beginCycle) return slow;
+  }
+}
+
+function findDuplicateCloned(nums: number[]): number {
+  let slow = 0;
+  let fast = 0;
 
   while (true) {
     slow = nums[slow];
