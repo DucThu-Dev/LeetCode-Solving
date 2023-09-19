08d9908e0d44f983ff1e2ea0f10bf6c8a18a57eb
Duc Thu <tranducthu.99@gmail.com>
2023-09-19 23:23:20 +0700

    [LeetCode] Another accepted solution 263

--- L1_999/263.UglyNumber.ts
+++ L1_999/263.UglyNumber.ts
@@ -20,3 +20,18 @@ function isUgly(n: number): boolean {
   if (Math.abs(n) >= 7) return false;
   return true;
 };
+
+function isUglyCloned(n: number): boolean {
+  if (n <= 0) return false;
+  while (n % 2 === 0) {
+    n /= 2;
+  }
+  while (n % 3 === 0) {
+    n /= 3;
+  }
+  while (n % 5 === 0) {
+    n /= 5;
+  }
+
+  return n === 1;
+};
