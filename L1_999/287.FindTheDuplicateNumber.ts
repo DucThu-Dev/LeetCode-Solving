af032ca85247063c8fbb8bafc318c5caa1c24a5e
Duc Thu <tranducthu.99@gmail.com>
2023-09-19 18:11:05 +0700

    [LeetCode] Solution 287 cloned

---
+++ L1_999/287.FindTheDuplicateNumber.ts
@@ -0,0 +1,24 @@
+function findDuplicate(nums: number[]): number {
+  let slow = 0;
+  let fast = 0;
+
+  while (true) {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+
+    if (slow === fast) {
+      break;
+    }
+  }
+
+  let beginCycle = 0;
+
+  while (true) {
+    slow = nums[slow];
+    beginCycle = nums[beginCycle];
+
+    if (beginCycle === slow) {
+      return beginCycle;
+    }
+  }
+}
