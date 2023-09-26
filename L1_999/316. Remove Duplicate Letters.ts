5ba0722fe770e596e5103eac8e8d77d48dd13e5c
Duc Thu <tranducthu.99@gmail.com>
2023-09-26 17:28:58 +0700

    [LeetCode] Practice 316 base on solution cloned

--- L1_999/316. Remove Duplicate Letters.ts
+++ L1_999/316. Remove Duplicate Letters.ts
@@ -1,4 +1,33 @@
-function removeDuplicateLetters(s: string): string {
+function removeDuplicateLettersPractice(s: string): string {
+  let ans = '';
+  const lastExist = new Array(26).fill(0);
+  const answerExist = new Array(26).fill(false);
+
+  function charIndex(char: string): number {
+    return char.charCodeAt(0) - 97;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    lastExist[charIndex(s[i])] = i;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (answerExist[charIndex(s[i])]) continue;
+    while (ans.length &&
+      ans.charCodeAt(ans.length - 1) > s.charCodeAt(i) &&
+      lastExist[charIndex(ans[ans.length - 1])] > i) {
+      answerExist[charIndex(ans[ans.length - 1])] = false;
+      ans = ans.slice(0, -1);
+    }
+
+    ans += s[i];
+    answerExist[charIndex(s[i])] = true;
+  }
+
+  return ans;
+}
+
+function removeDuplicateLettersCloned(s: string): string {
   let ans = '';
   const lastPosition: number[] = new Array(26).fill(0);
   const check: boolean[] = new Array(26).fill(false);
