e2823ed419e88c8cb4256cdf0759a2194aac1f36
Duc Thu <tranducthu.99@gmail.com>
2023-10-02 15:22:33 +0700

    [LeetCode] Solution for 62

--- L1_999/62. Unique Paths.ts
+++ L1_999/62. Unique Paths.ts
@@ -12,3 +12,25 @@ function uniquePathsOriginal(m: number, n: number): number {
     return moveDown + moveRight;
   }
 };
+
+function uniquePaths(m: number, n: number): number {
+  const rem = new Map<number, Map<number, number>>();
+  return travel(0, 0);
+
+  function travel(x: number, y: number): number {
+    if (x >= m || y >= n) return 0;
+    if (x === m - 1 && y === n - 1) return 1;
+
+    if (rem.has(m - x) && rem.get(m - x)!.has(n - y)) return rem.get(m - x)!.get(n - y)!;
+    let result = 0;
+    result += travel(x, y + 1);
+    result += travel(x + 1, y);
+
+
+    if (!rem.has(m - x)) {
+      rem.set(m - x, new Map<number, number>())
+    }
+    rem.get(m - x)!.set(n - y, result);
+    return result;
+  }
+};
