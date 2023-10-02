aca3f38bd54aae958ea25ef80fdcd51c92d2c912
Duc Thu <tranducthu.99@gmail.com>
2023-10-02 11:55:21 +0700

    [LeetCode] Cloned solution stack based 456

--- L1_999/456. 132 Pattern.ts
+++ L1_999/456. 132 Pattern.ts
@@ -1,4 +1,4 @@
-function find132pattern(nums: number[]): boolean {
+function find132patternOriginal(nums: number[]): boolean {
   for (let i1 = 0; i1 < nums.length - 2; i1++) {
     for (let i2 = i1 + 1; i2 < nums.length - 1; i2++) {
       if (nums[i2] > nums[i1]) {
@@ -12,3 +12,22 @@ function find132pattern(nums: number[]): boolean {
   }
   return false;
 };
+
+/// Checking solution here.
+/// https://leetcode.com/problems/132-pattern/solutions/4107421/99-35-stack-left-approach-binary-search/
+
+/// Stack based.
+/// Example: [1,2,3,4], [3,1,4,2]
+function find132pattern(nums: number[]): boolean {
+  const stack = new Array<number>();
+  let third = -Infinity;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] < third) return true;
+    while (stack.length > 0 && stack[stack.length - 1] < nums[i]) {
+      third = stack.pop()!;
+    }
+    stack.push(nums[i]);
+  }
+  return false;
+}
