4087b8a31ef62bcaea5d8bf3247e44a48a5df504
Duc Thu <tranducthu.99@gmail.com>
2023-10-04 21:14:19 +0700

    [LeetCode] Solution for 706

---
+++ L1_999/706. Design HashMap.ts
@@ -0,0 +1,37 @@
+class MyHashMap {
+  ENTRIES_SIZE: number = 500
+
+  private entries = new Array<Array<Array<number>>>(this.ENTRIES_SIZE);
+
+  put(key: number, value: number): void {
+    let keyIndex = this.hash(key);
+    if (this.entries[keyIndex]) {
+      let entryIndex = this.entries[keyIndex].findIndex(entry => entry[0] === key);
+      if (entryIndex >= 0) this.entries[keyIndex][entryIndex][1] = value;
+      else this.entries[keyIndex].push([key, value]);
+    } else {
+      this.entries[keyIndex] = new Array<Array<number>>(1).fill([key, value])
+    }
+  }
+
+  get(key: number): number {
+    let keyIndex = this.hash(key)
+    if (this.entries[keyIndex]) {
+      let entryIndex = this.entries[keyIndex].findIndex(entry => entry[0] === key);
+      if (entryIndex >= 0) return this.entries[keyIndex][entryIndex][1]
+    }
+    return -1;
+  }
+
+  remove(key: number): void {
+    let keyIndex = this.hash(key)
+    if (this.entries[keyIndex]) {
+      let entryIndex = this.entries[keyIndex].findIndex(entry => entry[0] === key);
+      if (entryIndex >= 0) this.entries[keyIndex][entryIndex][1] = -1
+    }
+  }
+
+  hash(key: number): number {
+    return key % this.ENTRIES_SIZE
+  }
+}
