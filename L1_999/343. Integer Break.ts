12220a1a09753a6a795d8188bd34d552a42fe4cc
Duc Thu <tranducthu.99@gmail.com>
2023-10-06 11:34:54 +0700

    [LeetCode ] Best solution but I have no idea 343

--- L1_999/343. Integer Break.ts
+++ L1_999/343. Integer Break.ts
@@ -39,3 +39,16 @@ function integerBreak2(n: number): number {
   if (remain === 2) return Math.pow(3, countOf3) * 2
   return Math.pow(3, countOf3);
 }
+
+/// Best solution
+function integerBreak(n: number): number {
+  const dp = new Array(n + 1).fill(0);
+
+  for (let i = 2; i <= n; i++) {
+      for (let j = 1; j < i; j++) {
+          dp[i] = Math.max(dp[i], j * Math.max(i - j, dp[i - j]));
+      }
+  }
+
+  return dp[n];
+}
