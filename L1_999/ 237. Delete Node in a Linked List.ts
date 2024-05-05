5a354955e8222ffd15cf8a58bfa23a1056afdebb
Duc Thu <tranducthu.99@gmail.com>
2024-05-05 22:07:16 +0700

    Solution better for 237

--- L1_999/ 237. Delete Node in a Linked List.ts
+++ L1_999/ 237. Delete Node in a Linked List.ts
@@ -19,3 +19,8 @@ function deleteNode(node: ListNode | null): void {
     node.next = null;
   }
 };
+
+function deleteNodeBetter(node: ListNode | null): void {
+  node!.val = node!.next!.val!
+  node!.next = node!.next!.next
+};
