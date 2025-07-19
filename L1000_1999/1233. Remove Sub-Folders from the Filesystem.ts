795c3c637dc9a424a66c6da6ad415e6cd9afc9f1
Duc Thu <tranducthu.99@gmail.com>
2025-07-19 23:30:17 +0700

    Solution 2 1233

--- L1000_1999/1233. Remove Sub-Folders from the Filesystem.ts
+++ L1000_1999/1233. Remove Sub-Folders from the Filesystem.ts
@@ -34,3 +34,25 @@ function removeSubfolders(folder: string[]): string[] {
 
   return folder;
 }
+
+function removeSubfolders2(folder: string[]): string[] {
+  folder.sort((a, b) => a.length - b.length);
+
+  let n = folder.length;
+  for (let i = 0; i < n; i++) {
+    const current = folder[i];
+    for (let j = i + 1; j < n; j++) {
+      const checking = folder[j];
+      if (
+        checking.startsWith(current) &&
+        (checking.length === current.length || checking[current.length] === "/")
+      ) {
+        folder.splice(j, 1);
+        j--;
+        n--;
+      }
+    }
+  }
+
+  return folder;
+}
